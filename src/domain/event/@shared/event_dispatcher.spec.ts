76c4ced838b76027c5783d1279dae2e4902ea6fe
Leo Silva <leocardosodev@gmail.com>
2023-09-21 12:34:03 -0300

    feat: add notify method to EventDispatcher

--- src/domain/event/@shared/event_dispatcher.spec.ts
+++ src/domain/event/@shared/event_dispatcher.spec.ts
@@ -1,4 +1,5 @@
 import SendEmailWhenProductIsCreatedHandler from "../product/handler/send_email_when_product_is_created";
+import ProductCreatedEvent from "../product/product_created_event";
 import EventDispatcher from "./event_dispatcher";
 
 describe("Domain events test", () => {
@@ -28,6 +29,21 @@ describe("Domain events test", () => {
     expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"][0]).toMatchObject(eventHandler);
     eventDispatcher.unregisterAll();
     expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"]).toBeUndefined();
+  });
+
+  it("Should notify all event handlers", () => {
+    const eventDispatcher = new EventDispatcher();
+    const eventHandler = new SendEmailWhenProductIsCreatedHandler();
+    const spyEventHandler = jest.spyOn(eventHandler, "handle");
+    eventDispatcher.register("ProductCreatedEvent", eventHandler);
+    expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"][0]).toMatchObject(eventHandler);
+    const productCreatedEvent = new ProductCreatedEvent({
+      name: "Product 1",
+      description: "Product 1 description",
+      price: 10.0,
+    });
+    eventDispatcher.notify(productCreatedEvent);
+    expect(spyEventHandler).toBeCalled();
   });
 
 });
--- src/domain/event/product/handler/send_email_when_product_is_created.ts
+++ src/domain/event/product/handler/send_email_when_product_is_created.ts
@@ -3,6 +3,6 @@ import ProductCreatedEvent from "../product_created_event";
 
 export default class SendEmailWhenProductIsCreatedHandler implements EventHandlerInterface<ProductCreatedEvent>{
   handle(event: ProductCreatedEvent): void {
-    console.log(`Sending email to ${event} .....`);
+    console.log(`Sending email to ${event.eventData.name} .....`);
   }
 }
